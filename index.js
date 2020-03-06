40c89454e6ea6e80b8f56198829437d54427b49d
gauravko <gaurav.gprs@gmail.com>
2020-03-06 12:39:56 +0530

    v1

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/home',
+        handler: (request, h) => {
+
+            return {
+                banners : [
+                    {
+                        img_url     : 'http://somelink.jpg',
+                        redirect    : true,
+                        route       : '/contests' 
+                    },
+                    {
+                        img_url     : 'http://somelink.jpg',
+                        redirect    : true,
+                        route       : '/transactions' 
+                    },
+                    {
+                        img_url     : 'http://somelink.jpg',
+                        redirect    : false,
+                        route       : null
+                    },
+                ],
+
+                challenges : [
+                    {
+                        
+                    }
+
+                ]
+            };
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+
+    return server;
+};
+
+init();
