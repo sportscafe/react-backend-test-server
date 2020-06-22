19b673fb8f59148610e2e3393db8c994b217d69b
6192dp <prasad.mvss1@gmail.com>
2020-06-22 17:55:30 +0530

    hapi accepting cors

--- build/index.js
+++ build/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _routes = require("./src/routes");
 
@@ -6,30 +6,35 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var Hapi = require('@hapi/hapi');
+var Hapi = require("@hapi/hapi");
 
 var init = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator(function* () {
     var server = Hapi.server({
       port: 3000,
-      host: 'localhost'
+      host: "localhost",
+      routes: {
+        cors: {
+          origin: ["*"]
+        }
+      }
     });
     var routes = [{
-      method: 'GET',
-      path: '/home',
+      method: "GET",
+      path: "/home",
       handler: (request, h) => (0, _routes.home)()
     }, {
-      method: 'GET',
-      path: '/wallet',
+      method: "GET",
+      path: "/wallet",
       handler: (request, h) => (0, _routes.wallet)()
     }, {
-      method: 'GET',
-      path: '/contest',
+      method: "GET",
+      path: "/contest",
       handler: (request, h) => (0, _routes.contests)()
     }];
     server.route(routes);
     yield server.start();
-    console.log('Server running on %s', server.info.uri);
+    console.log("Server running on %s", server.info.uri);
     return server;
   });
 
